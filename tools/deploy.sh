cfb4c92c1a0204a60ef860ebb0227d97a0c18a58
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-12-28 19:10:24 +0100

    Updated and adjusted deploy

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -10,60 +10,9 @@ else
     echo "Prepering build for version $VERSION_TAG"
 fi
 
-
 ScriptPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 RootPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 
-modname="Tiberium"
-
 cd $RootPath
 
-[[ ! -d release/$modname ]] && mkdir -p release/$modname
-
-echo "Prepering build..."
-
-for filePath in $(find); do
-    # Dont copy .
-    if [[ "$filePath" == "." ]]; then continue; fi;
-    # Directories
-    if [[ "$(echo "$filePath" | awk -F "/" '{print $2}')" == "." ]]; then continue; fi;
-    if [[ "$(echo "$filePath" | awk -F "/" '{print $2}')" == ".git" ]]; then continue; fi;
-    if [[ "$(echo "$filePath" | awk -F "/" '{print $2}')" == ".github" ]]; then continue; fi;
-    if [[ "$(echo "$filePath" | awk -F "/" '{print $2}')" == "release" ]]; then continue; fi;
-    if [[ "$(echo "$filePath" | awk -F "/" '{print $2}')" == "tools" ]]; then continue; fi;
-    if [[ "$(echo "$filePath" | awk -F "/" '{print $2}')" == "resources" ]]; then continue; fi;
-    # Files
-    #if [[ "$(echo "$filePath" | awk -F "/" '{print $2}')" == "Tiberium.mod" ]]; then continue; fi;
-    # File types
-    if [[ "$(echo $(basename $filePath) | awk -F "." '{print $2}')" == "psd" ]]; then continue; fi;
-    if [[ "$(echo $(basename $filePath) | awk -F "." '{print $2}')" == "png" ]]; then continue; fi;
-    if [[ "$(echo $(basename $filePath) | awk -F "." '{print $2}')" == "zip" ]]; then continue; fi;
-    
-    [[ -f $release/$modname/$filePath ]] && rm release/$modname/$filePath
-    echo Copying $filePath
-    echo "$filePath" | cpio -p release/$modname/ >/dev/null 2>&1
-
-done
-
-
-echo "Setting up documents..."
-
-echo "./thumbnail.png" | cpio -p release/$modname/ >/dev/null 2>&1
-
-sed -i "s/0.0.0/$VERSION_TAG/g" release/$modname/descriptor.mod
-sed -i "s/DevBuild/$VERSION_TAG/g" release/$modname/descriptor.mod
-
-sed -i "s/0.0.0/$VERSION_TAG/g" release/$modname/Tiberium.mod
-sed -i "s/path=\"mod\/Tiberium_DevBuild\"/path=\"mod\/Tiberium\"/g" release/$modname/Tiberium.mod
-sed -i "s/DevBuild/$VERSION_TAG/g" release/$modname/Tiberium.mod
-
-sed -i "s/# Tiberium Spread Mechanic/# Tiberium Spread Mechanic $VERSION_TAG/g" release/$modname/README.md
-cat release/$modname/README.md | sed -i '/<p/{:a;N;/p>/!ba};//d' release/$modname/README.md
-
-echo "Creating Release..."
-cd release
-[[ -f $modname-$VERSION_TAG.zip ]] && rm $modname-$VERSION_TAG.zip
-mv $modname/Tiberium.mod Tiberium.mod
-
-zip $modname-$VERSION_TAG.zip -r $modname Tiberium.mod
-mv Tiberium.mod $modname/Tiberium.mod
+python3 ./tools/build.py $VERSION_TAG
