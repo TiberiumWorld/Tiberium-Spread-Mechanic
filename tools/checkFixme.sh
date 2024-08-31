5ea8381739e46c0a5cb3c8d7976261d626bc0de4
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-08-31 11:00:36 +0200

    exclude better

--- tools/checkFixme.sh
+++ tools/checkFixme.sh
@@ -3,7 +3,7 @@
 SCRIPTPATH=`dirname $(readlink -f $0)`
 cd $SCRIPTPATH/..
 
-exclude="--exclude-dir=tools"
+exclude="--exclude-dir=tools --exclude-dir=.github --exclude-dir=.git"
 
 grep -rn "FIXME" $exclude
 [[ ! $(grep -rn "FIXME" $exclude | wc -l) == 0 ]] && echo "ERROR: FIXME found" && exit 1
--- tools/checkTodo.sh
+++ tools/checkTodo.sh
@@ -3,7 +3,7 @@
 SCRIPTPATH=`dirname $(readlink -f $0)`
 cd $SCRIPTPATH/..
 
-exclude="--exclude-dir=tools"
+exclude="--exclude-dir=tools --exclude-dir=.github --exclude-dir=.git"
 
 grep -rn "TODO" $exclude
 [[ ! $(grep -rn "TODO" $exclude | wc -l) == 0 ]] && echo "WARNING: TODO found" && exit 0
